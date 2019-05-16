f5983f03b50cc921d5cf1d5271bb17f605d91cf1
namnh06 <namnh.developer@gmail.com>
2019-05-16 21:19:10 +0700

    finish ss8

---
+++ html/08-20190516/bong-den/script.js
@@ -0,0 +1,5 @@
+function batBongDen() {
+  console.log('hello');
+  document.getElementById('image').src = "pic_bulbon.gif";
+  console.log('chay xong chua?');
+}
---
+++ html/08-20190516/demo/script.js
@@ -0,0 +1,38 @@
+// var a = 'xin chao cac ban';
+// var b = 5;
+// var c = 6;
+// var d = b + c;
+// var ten = 'Nguyen Hai Nam';
+// var lop = 'Aptech PHP 18';
+// var thongBao = ten + lop;
+// // alert(d);
+// console.log(thongBao);
+// hienThiKetQua();
+// console.log('aaa');
+
+// function hienThiKetQua() {
+//   var a = 5;
+//   var b = 7;
+//   console.log(a + b);
+// }
+
+// invoke - call
+
+// function quetNha() {
+//   // 1. lay choi
+//   // 2. lay hot rac
+//   // 3. quet
+// }
+
+
+// DOM
+// console.log(document.getElementById('demo'));
+function thongBao() {
+  alert('hello world');
+}
+
+function hienThiKetQua() {
+  document.getElementById('demo').innerHTML = 'Toi La Nam';
+
+  document.getElementById('lop').innerHTML = 'PHP 18';
+}
