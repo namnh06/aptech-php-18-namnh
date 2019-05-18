3d523da421ff5d4fb4ba71a126ff108e95530993
namnh06 <namnh.developer@gmail.com>
2019-05-18 21:14:46 +0700

    done ss9

--- html/09-20190518/carousel/script.js
+++ html/09-20190518/carousel/script.js
@@ -1,19 +1,30 @@
+// setInterval(function () {
+//   chuyenSlide();
+// }, 2000);
+
+// setInterval(chuyenSlide, 2000);
+
 var hinhMot = document.getElementById('hinh-1');
 var hinhHai = document.getElementById('hinh-2');
 var hinhBa = document.getElementById('hinh-3');
 
 function chuyenSlide() {
+
   if (hinhMot.style.display == 'block') {
     hinhMot.style.display = 'none';
     hinhHai.style.display = 'block';
-    hinhBa.style.display = 'none';
+    // hinhBa.style.display = 'none';
   } else if (hinhHai.style.display == 'block') {
-    hinhMot.style.display = 'none';
+    // hinhMot.style.display = 'none';
     hinhHai.style.display = 'none';
     hinhBa.style.display = 'block';
   } else {
-    hinhMot.style.display = 'block';
-    hinhHai.style.display = 'none';
     hinhBa.style.display = 'none';
+    hinhMot.style.display = 'block';
+    // hinhHai.style.display = 'none';
   }
+
+  setTimeout(chuyenSlide, 2000);
 }
+
+setTimeout(chuyenSlide, 2000);
---
+++ html/09-20190518/javascript/script.js
@@ -0,0 +1,16 @@
+console.log('Xin chao ca lop');
+var h1Element = document.getElementById('loi-chao');
+console.log(h1Element);
+
+console.log(h1Element.innerHTML);
+
+
+
+var a = 1000;
+var b = 2000;
+console.log(a + b);
+
+function hienThiConsole() {
+  console.log('Dong 14 thuc thi !!!');
+  h1Element.innerHTML = 'Xin chao PHP 18';
+}
