e839dacc1ed81cdb17b098e2a800722a2444dafd
namnh-nick <nam.nguyenhai@yuriqa.com>
2019-06-19 15:29:04 +0700

    Updated homework ss3

---
+++ php/04-20190619/homework.sql
@@ -0,0 +1,44 @@
+USE test;
+
+CREATE TABLE posts(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    title varchar(255)
+);
+
+
+INSERT INTO posts(title)
+VALUES ('test 1') ,('test 2'), ('test 3'), ('test 4'), ('test 5');
+
+
+CREATE TABLE categories(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255)
+);
+
+
+INSERT INTO categories(name)
+VALUES ('thoi su'), ('kinh te'), ('something');
+
+
+
+CREATE TABLE category_post(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT,
+    category_id INT,
+    UNIQUE (post_id, category_id),
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+
+INSERT INTO category_post (post_id, category_id)
+VALUES (1,1), (1,2),(2,1),(1,3),(2,2);
+
+
+SELECT * FROM category_post ORDER BY id;  
+
+SELECT A.id, A.post_id, A.category_id, B.title, C.name FROM 
+category_post as A
+JOIN posts as B ON A.post_id = B.id
+JOIN categories as C ON A.category_id = C.id
+WHERE A.post_id = 1;
